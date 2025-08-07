750fe0b87181e9f2536518ab41606e642eed1903
ayushpatel25-prog <ayushsbr09@gmail.com>
2025-08-07 06:15:30 +0000

    fixed routing issue

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/myanportfolio',
-  assetPrefix: '/myanportfolio/',
   typescript: {
     ignoreBuildErrors: true,
   },
